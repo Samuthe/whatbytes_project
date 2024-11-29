d337cb4c8e1ae649349e374d3e621d79181920d5
Samuthe <ekatisamuel@gmail.com>
2024-11-29 12:35:05 +0300

    added urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,7 @@ from . import views
 """The URL configuration for the accounts app. This file is used to define the URL patterns for the views in the accounts app. 
 The views are imported from the views.py file in the accounts app. The urlpatterns list routes URLs to views. The path() function is used to define the URL patterns. The first argument is the URL pattern, the second argument is the view function, and the third argument is the name of the URL pattern. The name of the URL pattern is used to refer to the URL pattern in templates and other parts of the Django project."""
 urlpatterns = [
+    path('', views.home_view, name='home'),
     path('login/', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
     path('forgot-password/', views.forgot_password_view, name='forgot_password'),
