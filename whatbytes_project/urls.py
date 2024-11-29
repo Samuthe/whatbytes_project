fb572ed9a6a4abe2f53d955c2c695e6b30d92e45
Samuthe <ekatisamuel@gmail.com>
2024-11-29 03:22:54 +0300

    The URL configuration for the whatbytes_project project

--- whatbytes_project/urls.py
+++ whatbytes_project/urls.py
@@ -14,9 +14,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+# from django.contrib import admin
+# from django.urls import path
 
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+# ]
+
+
+from django.contrib import admin
+from django.urls import path, include
+
+"""The URL configuration for the whatbytes_project project. This file is used to define the URL patterns for the views in the project. 
+The views are imported from the views.py file in the project. The urlpatterns list routes URLs to views. 
+The path() function is used to define the URL patterns. 
+The first argument is the URL pattern, the second argument is the view function, and the third argument is the name of the URL pattern. 
+The name of the URL pattern is used to refer to the URL pattern in templates and other parts of the Django project."""
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
 ]
+
